8253e85bb64d51b8f3989071d4ffa000e69b4082
Udaykumar95 <244g5a3316@srit.ac.in>
2025-09-13 16:05:48 +0530

    Delete javascript directory

--- javascript/neww.js
+++
@@ -1,7 +0,0 @@
-//functions
-function generateWelcomeMessage(name){
-    return "welcome ," + name + "! good morning";
-}
-
-let welcome_message = generateWelcomeMessage(give )
-console.log(welcome_message)
